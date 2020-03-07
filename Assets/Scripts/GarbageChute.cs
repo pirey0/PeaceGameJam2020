9e5b5cc5f863f8f529c2efbbdf4fd41fd835fb8c
marvellousmarvolo <la0gueule@gmail.com>
2020-03-07 16:10:40 +0100

    added Garbage Can prefab and animation

--- Assets/Scripts/GarbageChute.cs
+++ Assets/Scripts/GarbageChute.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GarbageChute : MonoBehaviour, IInteractable
 {
     [SerializeField] Transform target;  
-
+    [SerializeField] Animator animator;
     public bool CanInteractWith(PlayerController playerController){
         return true;
     }
@@ -15,7 +15,7 @@ public class GarbageChute : MonoBehaviour, IInteractable
             IPickupable garbage = playerController.CurrentPickup;
             playerController.ForceRelease();
             garbage.GetTransform().position = target.position;
-            
+            animator.SetTrigger("Open");
         }
         else{
             // Feedback nothing to throw away
